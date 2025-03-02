b51485ac5144c91315806c81d2862fc0c2d3754c
Akash Jaiswar <jaiswarakash04@gmail.com>
2025-03-03 02:50:10 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+  const express = require('express');
+  const cors = require('cors');
+  const connectDB = require('./config/db');
+  const userRoutes = require('./routes/user');
+  require('dotenv').config();
+  const ngrok = require('ngrok');
+  const app = express();
+
+  // Middleware
+  app.use(cors());
+  app.use(express.json());
+
+  // Connect to DB
+  connectDB();
+
+  // Routes
+  app.use('/users', userRoutes);
+
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, async() => {
+      console.log(`Server running on port ${PORT}`)
+      try {
+          const url = await ngrok.connect(PORT);
+          console.log(`🌍 ngrok Public URL: ${url}`);
+          console.log(`🔗 Use this URL in your frontend: ${url}`);
+        } catch (err) {
+          console.error("❌ ngrok Error:", err);
+        }
+  });
